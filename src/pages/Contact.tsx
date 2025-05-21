8b7c6ea04db31b8829475074f65ed0010e995be2
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-05-21 15:58:18 -0500

    Another troubleshooter

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -27,7 +27,7 @@ const Contact = () => {
     message: ""
   });
   const [loading, setLoading] = useState(false);
-  console.log(import.meta.env.VITE_EMAIL_PUBLIC_KEY);
+  console.log(import.meta.env.VITE_EMAIL_PUBLIC_KEY || 'your_public_key');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
